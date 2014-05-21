996a42080f1dbc1a8631ccf4c470c169e1b45c23
Behan Webster <behanw@converseincode.com>
2014-05-21 12:02:30 -0700

    LLVMLINUX: kbuild: Add better clang cross build support
    Add cross target to CC if using clang. Also add custom gcc toolchain path for
    fallback gcc tools.
    
    COMPILER is previously set to "clang" if CC=clang was set from the make command
    line.  So -target and -gcc-toolchain can be added to CC, since we already know
    that it is set.
    
    Clang will fallback to using things like ld, as, and libgcc if (respectively)
    one of the llvm linkers isn't available, the integrated assembler is turned
    off, or an appropriately cross-compiled version of compiler-rt isn't available.
    To this end, you can specify the path to this fallback gcc toolchain with
    GCC_TOOLCHAIN.
    
    Change-Id: If2fb067b45d9d104c139d22a48a6897a4f1b1bfb
    Signed-off-by: Behan Webster <behanw@converseincode.com>
    Reviewed-by: Jan-Simon Möller <dl9pf@gmx.de>
    Reviewed-by: Mark Charlebois <charlebm@gmail.com>
    Signed-off-by: Greg Hackmann <ghackmann@google.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- compile-script.sh
+++ compile-script.sh
@@ -1,5 +1,8 @@
-export ARCH=arm && export SUBARCH=arm
-make clean && make mrproper
-export CROSS_COMPILE=/home/andrea/TC/bin/arm-eabi-
-make lineageos_victara_defconfig
-make -j4
+	export ARCH=arm
+	export SUBARCH=arm
+	export CROSS_COMPILE="/home/andrea/TC/bin/arm-eabi-"
+	export LD_LIBRARY_PATH="/home/andrea/lolz_clang/lib"
+	echo "Performing a full clean..."
+	make mrproper CC=/home/andrea/lolz_clang/bin/clang HOSTCC=/home/andrea/lolz_clang/bin/clang
+	make clean CC=/home/andrea/lolz_clang/bin/clang HOSTCC=/home/andrea/lolz_clang/bin/clang
+    make lineageos_victara_defconfig CC=/home/andrea/lolz_clang/bin/clang HOSTCC=/home/andrea/lolz_clang/bin/clang
