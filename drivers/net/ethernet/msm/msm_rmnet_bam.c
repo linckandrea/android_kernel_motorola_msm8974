ed40077c568592805124f87f6cddd0f4b66a5780
myfluxi <linflux@arcor.de>
2016-01-16 15:04:34 +0100

    msm_rmnet_bam: Remove watchdog_timeo
    Change-Id: I05223934a285abc8f1dbf3a6b07b114327635937

--- drivers/net/ethernet/msm/msm_rmnet_bam.c
+++ drivers/net/ethernet/msm/msm_rmnet_bam.c
@@ -729,7 +729,8 @@ static void __init rmnet_setup(struct net_device *dev)
 	dev->needed_tailroom = TAILROOM;
 	random_ether_addr(dev->dev_addr);
 
-	dev->watchdog_timeo = 0;
+    dev->watchdog_timeo = 0;
+    
 }
 
 static struct net_device *netdevs[RMNET_DEVICE_COUNT];
