2fa4a19e1d3b24fe0b5431d6d442097b0fca9e25
linckandrea <linckandrea@outlook.com>
2019-08-31 17:45:05 +0200

    fix some derp for mpdecision

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -149,7 +149,7 @@ void start_bandwidth_timer(struct hrtimer *period_timer, ktime_t period)
 
 DEFINE_MUTEX(sched_domains_mutex);
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
-#if defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
+#ifdef (CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
 DEFINE_PER_CPU_SHARED_ALIGNED(struct nr_stats_s, runqueue_stats);
 #endif
 
@@ -2226,7 +2226,7 @@ unsigned long nr_iowait(void)
 	return sum;
 }
 
-#if defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
+#ifdef (CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
 unsigned long avg_nr_running(void)
 {
 	unsigned long i, sum = 0;
