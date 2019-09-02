67e1edf8bd17df101ca2f5fbb3807823e2724223
linckandrea <linckandrea@outlook.com>
2019-09-02 19:22:12 +0200

    lazyplug: remove unused function

--- drivers/cpufreq/lazyplug.c
+++ drivers/cpufreq/lazyplug.c
@@ -414,20 +414,6 @@ static void lazyplug_work_fn(struct work_struct *work)
 		msecs_to_jiffies(sampling_time));
 }
 
-static void wakeup_boost(void)
-{
-	unsigned int cpu;
-	struct cpufreq_policy *policy;
-	struct ip_cpu_info *l_ip_info;
-
-	for_each_online_cpu(cpu) {
-		policy = cpufreq_cpu_get(cpu);
-		l_ip_info = &per_cpu(ip_info, cpu);
-		policy->cur = l_ip_info->cur_max;
-		cpufreq_update_policy(cpu);
-	}
-}
-
 static unsigned int Lnr_run_profile_sel = 0;
 static bool Lprevious_state = false;
 void lazyplug_enter_lazy(bool enter)
