8a56f8af0b52948c7c3fa1073093f1276f30a71a
Andrea <linckandrea01@gmail.com>
2020-08-07 23:08:14 +0200

    blkdev: fix mismerge

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -432,8 +432,9 @@ struct request_queue {
 #define QUEUE_FLAG_SAME_FORCE  18	/* force complete on same CPU */
 #define QUEUE_FLAG_SANITIZE    19	/* supports SANITIZE */
 
-#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_STACKABLE) |		\
-                            (1 << QUEUE_FLAG_SAME_COMP))
+#define QUEUE_FLAG_DEFAULT  ((1 << QUEUE_FLAG_STACKABLE) |  \
+                            (1 << QUEUE_FLAG_SAME_COMP) |   \
+                            (0 << QUEUE_FLAG_ADD_RANDOM))
 
 static inline void queue_lockdep_assert_held(struct request_queue *q)
 {
