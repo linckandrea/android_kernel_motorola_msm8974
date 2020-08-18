efc76cd20fdc089ceb92585e4552e2bb1c32f4b5
Andrea <linckandrea01@gmail.com>
2020-08-18 09:43:37 +0200

    Revert "Revert "sched_avg: add run queue averaging.""
    This reverts commit 85ffd03892a04587f633045a1c7bf0f0f41f3da3.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -981,6 +981,7 @@ static inline unsigned int do_avg_nr_running(struct rq *rq)
 
 static inline void inc_nr_running(struct rq *rq)
 {
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
 	rq->nr_running++;
 
 	if (rq->nr_running >= 2) {
@@ -991,6 +992,7 @@ static inline void inc_nr_running(struct rq *rq)
 
 static inline void dec_nr_running(struct rq *rq)
 {
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
 	rq->nr_running--;
 }
 
